52ae6be2e0470cdf71ea96ddd39714e1746145ef
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-08-01 10:45:18 +1200

    Made a backgroup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
-<h1>JUJUTSU KAISEN</h1>
-<head>
-    <link rel="stylesheet" href="styles.scc">
-</head>
-<body>
-    <a href="https://www.instagram.com/ansonlsy931_/" target="_blank" class="instagram-button">
-        <img src="instagram-icon.png" alt="Instagram">
-    </a>
-</body>
+<!-- CSS -->
+<style>
+    body{
+        background-image: url("Gojo.mp4")
+    }
+</style>
 
-<!-- svelte-ignore a11y-media-has-caption -->
+<!-- HTML __>-->
 <video width="100%" autoplay muted>
     <source src="Gojo.mp4" type="video/mp4">
-</video>
+  </video>
+
+
+
