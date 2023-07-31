19a76fc2b0be3e551f5d3e9424293bbcb8244c78
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-08-01 10:11:36 +1200

    Updated the webpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>JUJUTSU KAISEN</h1>
+<head>
+    <link rel="stylesheet" href="styles.scc">
+</head>
+<body>
+    <a href="https://www.instagram.com/ansonlsy931_/" target="_blank" class="instagram-button">
+        <img src="instagram-icon.png" alt="Instagram">
+    </a>
+</body>
