199fdbdb220b4a43cc25f507b651766da2b5cb03
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-08-01 11:02:34 +1200

    Created the css file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <!-- CSS -->
 <style>
-    body{
-        background-image: url("Gojo.mp4")
+    :global(body){
+        background-color: lightblue
+    }
+    div{
+        background-color: purple;
+        opacity: 0.3;
     }
 </style>
 
 <!-- HTML __>-->
+<h3>JUJUTSU KAISEN</h3>
+<p></p>
+<h3>五条 悟</h3>
+<h1>GOJO SATORU</h1>
+
 <video width="100%" autoplay muted>
     <source src="Gojo.mp4" type="video/mp4">
   </video>
