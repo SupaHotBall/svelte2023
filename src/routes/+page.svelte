80bc70a6047bdeed27569f7ff4ad43496e871c69
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-08-01 11:47:52 +1200

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,19 @@
 <svelte:head><link rel="stylesheet" href="css/main.css" /></svelte:head>
 
 <!-- HTML __>-->
-<h3>JUJUTSU KAISEN</h3>
-<p></p>
-<h3>五条 悟</h3>
-<h1>GOJO SATORU</h1>
+
 
 <div class="video-wrapper">
     <video playsinline autoplay muted loop poster="image/Gojo.mp4">
       <source src="image/Gojo.mp4" type="video/mp4">
       Your browser does not support the video tag.
     </video>
+  
+    <div class="header">
+        <h3>JUJUTSU KAISEN</h3>
+        <p></p>
+        <h3>五条 悟</h3>
+        <h1>GOJO SATORU</h1>
+      <button>Im gay</button>
+    </div>
   </div>
