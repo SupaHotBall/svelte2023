a49cf54be8fbe01dbc6304cc1c19037115162cbf
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-08-04 09:55:21 +1200

    Updated the lastest verison

---
+++ src/lib/Header.svelte
@@ -0,0 +1,6 @@
+<div class="header">
+    <h3 class="with-icon">JUJUTSU KAISEN</h3>
+    <br> <br>
+    <h3>五条 悟</h3>
+    <h1>GOJO<br>SATORU</h1>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Header from '$lib/Header.svelte'
+</script>
+
+<Header/>
+
+
+<svelte:head><link rel="stylesheet" href="css/main.css" /></svelte:head>
+
+<slot/>
