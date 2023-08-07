efec70f145c48f74b797f88ad687a9ccac2349f4
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-08-07 16:50:56 +1200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <body class="body-text">
-    <h3>五条 悟</h3>
-    <h1>GOJO<br>SATORU</h1>
+  <h3>五条 悟</h3>
+  <h1>GOJO<br />SATORU</h1>
+
+  <div class="background-image">
+    <img src="image/Gojowallpaper.jpg" alt="Main Page WallPaper" />
+  </div>
 </body>
-
-<div class="background-image">
-    <img src="image/Gojowallpaper.jpg" alt="Main Page WallPaper">
-</div>
-
