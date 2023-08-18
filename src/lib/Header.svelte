21cd36ff7a9ddfcf7995b20f224242ecce1cd8a0
SupaHotBall <lees27@student.op.ac.nz>
2023-08-18 14:07:47 +1200

    add the button css

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,9 +3,27 @@
     <a class="logo-icon">JUJUTSU KAISEN</a>
 
     <div class="header-right">
-        <a href="/">Home</a>
-        <a href="/About">About</a>
-        <a href="/Anything">Anything</a>
+
+        <a href="/">
+            <button class="glow-on-hover" type="button">Home</button>
+        
+        <div class="dropdown">
+            <button class="glow-on-hover" type="button">Characters</button>
+            <div class="dropdown-content">
+                <a href="/character1">Gojo</a>
+                <a href="/character2">Ryomen</a>
+                <a href="/character3">Toji</a>
+            </div>
+        </div>
+
+        <a href="/info">
+            <button class="glow-on-hover" type="button">Info</button>
+        </a>
+
+        <a href="/about">
+            <button class="glow-on-hover" type="button">About</button>
+        </a>
+
     </div>
 
 </header>
