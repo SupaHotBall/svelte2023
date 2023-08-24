74d7fee2c7bd04eda4aaa5c09c02940438681803
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-08-25 09:47:05 +1200

    asdasds

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,13 +3,34 @@
     <a class="logo-icon">JUJUTSU KAISEN</a>
 
     <div class="header-right">
+        <footer>
+    
+            <a class="Instagram-icon icon">Instagram</a>
+            <a class="Discord-icon icon">Discord</a>
+            <a class="SnapChat-icon icon">SnapChat</a>
+        
+            <div class="footer-right">
+                <a class="OtagoPoly-icon">Otago Polytechnic</a>
+                <a class="Phone-icon">+64 03 477 3014</a>
+            </div>
+            
+        </footer>
 
         <a href="/">
             <button class="glow-on-hover" type="button">Home</button>
         </a>
         
-        <div class="dropdown">
-            <button class="glow-on-hover" type="button">Characters</button>            
+        <div class="characters-tab">
+            <input type="checkbox" id="active">
+            <label for="active" class="glow-on-hover">Character</label>
+            <label for="active" class="close">X</label>
+            <div class="character-menu">
+                <ul>
+                    <li><a href="/character1">Gojo</a></li>
+                    <li><a href="/character2">Royomen</a></li>
+                    <li><a href="/character3">Toji</a></li>
+                </ul>
+            </div>           
         </div>
 
         <a href="/info">
@@ -24,10 +45,6 @@
 
 </header>
 
-<div class="character-menu">
-    <div class="dropdown-content">
-        <a href="/character1">Gojo</a>
-        <a href="/character2">Ryomen</a>
-        <a href="/character3">Toji</a>
-    </div>
+<div>
+    
 </div>
