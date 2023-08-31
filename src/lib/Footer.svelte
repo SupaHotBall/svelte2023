12c7b045027b9dd481c015b66948ddc2a2f0b60c
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-09-01 09:53:30 +1200

    updated the logo

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,1 +1,35 @@
+<footer>
+    <div class="span-items">
 
+        <div>
+            <div class="instagram fa-stack fa-2x">
+                <i class="fa-solid fa-circle fa-stack-2x"></i>
+                <i class="fa-brands fa-instagram fa-stack-1x fa-inverse"></i>
+            </div>
+            <div>Instagram</div>
+        </div>
+
+        <div>
+            <div class="discord fa-stack fa-2x">
+                <i class="fa-solid fa-circle fa-stack-2x"></i>
+                <i class="fa-brands fa-discord fa-stack-1x fa-inverse"></i>
+            </div>
+            <div>Discord</div>
+        </div>
+
+        <div>
+            <div class="snapchat fa-stack fa-2x">
+                <i class="fa-solid fa-circle fa-stack-2x"></i>
+                <i class="fa-brands fa-snapchat fa-stack-1x fa-inverse"></i>
+            </div>
+            <div>snapchat</div>
+        </div>
+
+    </div>
+
+    <div class="footer-right">
+        <a class="OtagoPoly-icon">Otago Polytechnic</a>
+        <a class="Phone-icon">+64 03 477 3014</a>
+    </div>
+    
+</footer>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,18 +3,7 @@
     <a class="logo-icon">JUJUTSU KAISEN</a>
 
     <div class="header-right">
-        <footer>
-    
-            <a class="Instagram-icon icon">Instagram</a>
-            <a class="Discord-icon icon">Discord</a>
-            <a class="SnapChat-icon icon">SnapChat</a>
         
-            <div class="footer-right">
-                <a class="OtagoPoly-icon">Otago Polytechnic</a>
-                <a class="Phone-icon">+64 03 477 3014</a>
-            </div>
-            
-        </footer>
 
         <a href="/">
             <button class="glow-on-hover" type="button">Home</button>
