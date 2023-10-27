4816d088ec573bf74afab4321213892c1d9f2403
SupaHotBall <lees27@student.op.ac.nz>
2023-10-27 19:49:15 +1300

    Added a class for the h1 in Gojo page

--- src/routes/character1/+page.svelte
+++ src/routes/character1/+page.svelte
@@ -1,54 +1,56 @@
 <div class="Gojo-page-background">
-  <h1>Gojo</h1>
-  <h1>Satoru</h1>
-</div>
+  <div class="Name">
+    <h1>Gojo</h1>
+    <h1>Satoru</h1>
+  </div>
 
-<div class="Video-container">
-  <button class="left-button">
-    <i class="fa-solid fa-circle-arrow-left fa-2xl" />
-  </button>
+  <div class="Video-container">
+    <button class="left-button">
+      <i class="fa-solid fa-circle-arrow-left fa-2xl" />
+    </button>
 
-  <iframe
-    width="560"
-    height="315"
-    src="https://www.youtube.com/embed/SQc2jhbGT2k?si=1DGQq0olblMVibZ3"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowfullscreen
-  />
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/SQc2jhbGT2k?si=1DGQq0olblMVibZ3"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
 
-  <iframe
-    width="560"
-    height="315"
-    src="https://www.youtube.com/embed/fueqERLu-Fk?si=Apns2jVo_RotE1GL&amp;start=4"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowfullscreen
-  />
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/fueqERLu-Fk?si=Apns2jVo_RotE1GL&amp;start=4"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
 
-  <iframe
-    width="560"
-    height="315"
-    src="https://www.youtube.com/embed/ia6sRyT6ArE?si=fWr3a2GfZIYFC6nb&amp;start=4"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowfullscreen
-  />
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/ia6sRyT6ArE?si=fWr3a2GfZIYFC6nb&amp;start=4"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
 
-  <iframe
-    width="560"
-    height="315"
-    src="https://www.youtube.com/embed/pzIbP2AUXy8?si=SyqzzAsuVhhSISg2&amp;start=4"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowfullscreen
-  />
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/pzIbP2AUXy8?si=SyqzzAsuVhhSISg2&amp;start=4"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
 
-  <button class="right-button">
-    <i class="fa-solid fa-circle-arrow-right fa-2xl" />
-  </button>
+    <button class="right-button">
+      <i class="fa-solid fa-circle-arrow-right fa-2xl" />
+    </button>
+  </div>
 </div>
