9247b23e808315668079a690bbe20d97fe7299f1
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-11-02 15:16:47 +1300

    added some video javascript in  my gojo page

--- src/routes/character1/+page.svelte
+++ src/routes/character1/+page.svelte
@@ -1,3 +1,36 @@
+<script>
+  let videoIndex = 0;
+
+  function scrollRight() {
+    videoIndex++;
+    if (videoIndex >= 2) { // Adjust this based on the number of videos
+      videoIndex = 2;
+    }
+  }
+
+  function scrollLeft() {
+    videoIndex--;
+    if (videoIndex < 0) {
+      videoIndex = 0;
+    }
+  }
+
+  function getVideoUrl(index) {
+    const videoUrls = [
+      "https://www.youtube.com/embed/SQc2jhbGT2k?si=1DGQq0olblMVibZ3",
+      "https://www.youtube.com/embed/fueqERLu-Fk?si=Apns2jVo_RotE1GL&start=4",
+      "https://www.youtube.com/embed/ia6sRyT6ArE?si=fWr3a2GfZIYFC6nb&start=4",
+      "https://www.youtube.com/embed/pzIbP2AUXy8?si=SyqzzAsuVhhSISg2&start=4"
+    ];
+
+    if (index >= 0 && index < videoUrls.length) {
+      return videoUrls[index];
+    } else {
+      return ""; // Return an empty URL for invalid indices
+    }
+  }
+</script>
+
 <div class="Gojo-page-background">
   <div class="Name">
     <h1>Gojo</h1>
@@ -5,62 +38,35 @@
   </div>
 
   <div class="Video-container">
-    <button class="left-button">
+    <button on:click={scrollLeft} class="left-button">
       <i class="fa-solid fa-circle-arrow-left fa-2xl"></i>
     </button>
 
     <div class="video-scroll-container">
-      <div class="video-wrapper">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/SQc2jhbGT2k?si=1DGQq0olblMVibZ3"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-      </div>
-
-      <div class="video-wrapper">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/fueqERLu-Fk?si=Apns2jVo_RotE1GL&amp;start=4"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-      </div>
-
-      <div class="video-wrapper">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/ia6sRyT6ArE?si=fWr3a2GfZIYFC6nb&amp;start=4"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-      </div>
-
-      <div class="video-wrapper">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/pzIbP2AUXy8?si=SyqzzAsuVhhSISg2&amp;start=4"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-      </div>
+      {#each [1, 2, 3, 4] as video, i}
+        {#if i >= videoIndex && i < videoIndex + 3}
+          <div class="video-wrapper">
+            <iframe
+              width="560"
+              height="315"
+              src={getVideoUrl(i)}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+        {/if}
+      {/each}
     </div>
 
-    <button class="right-button">
+    <button on:click={scrollRight} class="right-button">
       <i class="fa-solid fa-circle-arrow-right fa-2xl"></i>
     </button>
   </div>
 </div>
+
+
+
+
+
