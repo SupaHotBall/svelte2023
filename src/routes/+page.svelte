6a6cb7cc33dc03e740e4aaf199a255b2672a09cd
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-11-02 17:47:32 +1300

    fetching data from the api is done just need some css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,38 @@
-<svelte:head>
-  <title>
-      Home Page
-  </title>
-</svelte:head>
+<script>
+  import { onMount } from 'svelte';
+  let player;
 
+  onMount(() => {
+    // Load the YouTube iframe API and create the player when the component is mounted
+    const script = document.createElement('script');
+    script.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(script);
 
+    script.onload = () => {
+      window.onYouTubeIframeAPIReady = () => {
+        player = new YT.Player('player', {
+          height: '90%',
+          width: '90%',
+          videoId: 'PKHQuQF1S8k', // Replace with your video ID
+          events: {
+            'onReady': onPlayerReady,
+          },
+        });
+      };
+    };
+  });
+
+  // Function to start playing the video when it's ready
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+</script>
 
 <div class="HomePagebase-background">
   <div class="body-image"></div>
 </div>
+
+<div id="videoContainer">
+  <div id="player"></div>
+</div>
+
--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,51 +1,43 @@
 <script>
-    let regularImageUrl = '';
-  
-    // Define the query parameters for the Unsplash API request
-    const query = 'anime'; // You can adjust the query as needed
-    const clientId = 'U3Yj9H-A7xjn37lRamx6_TA0X0hBrpmVhWEHASXf7yc'; // Replace with your actual API key
-  
-    // Construct the API request URL
-    const apiUrl = `https://api.unsplash.com/search/photos?query=${query}`;
-  
-    // Fetch the data and update the regularImageUrl
-    fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Client-ID ${clientId}`
-      }
-    })
-    .then(response => {
+  let regularImageUrl = "";
+
+  // Define the query parameters for the Unsplash API request
+  const query = "anime"; // You can adjust the query as needed
+  const clientId = "U3Yj9H-A7xjn37lRamx6_TA0X0hBrpmVhWEHASXf7yc"; // Replace with your actual API key
+
+  // Construct the API request URL
+  const apiUrl = `https://api.unsplash.com/search/photos?query=${query}`;
+
+  // Fetch the data and update the regularImageUrl
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Client-ID ${clientId}`,
+    },
+  })
+    .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
-        throw new Error('API request failed');
+        throw new Error("API request failed");
       }
     })
-    .then(data => {
+    .then((data) => {
       if (data && data.results && data.results.length > 0) {
         regularImageUrl = data.results[0].urls.regular;
       } else {
-        console.error('No results found in the API response.');
+        console.error("No results found in the API response.");
       }
     })
-    .catch(error => {
-      console.error('Error fetching data:', error);
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
-  </script>
-
-
+</script>
 
 <div class="about-page">
-    <h1>About Page</h1>
-    <!-- Display the regular-sized image -->
+  <h1>About Page</h1>
+  <!-- Display the regular-sized image -->
+  <div class="About-Page-Background">
     <img src={regularImageUrl} alt="Anime Background" />
-    <p>Your about page content goes here...</p>
   </div>
-  In this modified code, we initialize the regularImageUrl variable to an empty string and then update it with the URL of the regular-sized image from the API response. We use an <img> element to display the image on your About page.
-  
-  
-  
-  
-  
-  
+</div>
