8924184a88efa439e61c4c38915ba2f23c2b36d6
SupaHotBall <lees27@student.op.ac.nz>
2023-11-08 16:31:38 +1300

    trying to make the ifarm video autoplay but it fails

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,11 +21,9 @@
       <iframe
         width="1000"
         height="700"
-        src="https://www.youtube.com/embed/PKHQuQF1S8k?si=81o3OE1QGKAMGXB7"
+        src="https://www.youtube.com/embed/PKHQuQF1S8k?si=81o3OE1QGKAMGXB7?autoplay=1&mute=1"
         title="YouTube video player"
         frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
       />
     </div>
     <div class="close-button glow-on-hover" on:click={closeVideo}>Close</div>
