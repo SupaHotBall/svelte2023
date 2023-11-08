cec1b5005ca6765103edfd0d874b355e863d5d92
SupaHotBall <lees27@student.op.ac.nz>
2023-11-08 16:36:05 +1300

    tryied again with the ?&autoplay=1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,14 +17,17 @@
 </div>
 
 <div id="container">
-    <div id="player">
-      <iframe
-        width="1000"
-        height="700"
-        src="https://www.youtube.com/embed/PKHQuQF1S8k?si=81o3OE1QGKAMGXB7?autoplay=1&mute=1"
-        title="YouTube video player"
-        frameborder="0"
-      />
-    </div>
-    <div class="close-button glow-on-hover" on:click={closeVideo}>Close</div>
+  <div id="player">
+    <iframe
+      width="1000"
+      height="700"
+      src="https://www.youtube.com/embed/PKHQuQF1S8k?si=81o3OE1QGKAMGXB7&autoplay=1"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
   </div>
+  <div class="close-button glow-on-hover" on:click={closeVideo}>Close</div>
+</div>
+
