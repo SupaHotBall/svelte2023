c478a976894f60bb37f575ca038c6dbe312d5ba0
SupaHotBall <lees27@student.op.ac.nz>
2023-11-08 16:46:17 +1300

    remove the mute=0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
     <iframe
       width="560"
       height="315"
-      src="https://www.youtube-nocookie.com/embed/PKHQuQF1S8k?si=GJ7JnnxnnJXXwQZN&amp?&autoplay=1&mute=0"
+      src="https://www.youtube-nocookie.com/embed/PKHQuQF1S8k?si=GJ7JnnxnnJXXwQZN&amp?&autoplay=1"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
