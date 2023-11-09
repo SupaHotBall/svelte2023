c1dfb124af7ec26576826f9ddc70205f8fdf9244
SupaHotBall <lees27@student.op.ac.nz>
2023-11-09 19:50:47 +1300

    fixing the nav button

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,59 +1,58 @@
 <script>
     var counter = 1;
-    setInterval(function() {
-        document.getElementById('radio' + counter).checked = true;
+    setInterval(function () {
+        document.getElementById("radio" + counter).checked = true;
         counter++;
-        if(counter > 5){
+        if (counter > 5) {
             counter + 1;
         }
     }, 5000);
 </script>
 
+<div class="Black-ground">
+    <div class="slider">
+        <div class="slides">
+            <input type="radio" name="radio-btn" id="radio1" />
+            <input type="radio" name="radio-btn" id="radio2" />
+            <input type="radio" name="radio-btn" id="radio3" />
+            <input type="radio" name="radio-btn" id="radio4" />
+            <input type="radio" name="radio-btn" id="radio5" />
 
-<div class="slider">
-    <div class="slides">
-        <input type="radio" name="radio-btn" id="radio1" />
-        <input type="radio" name="radio-btn" id="radio2" />
-        <input type="radio" name="radio-btn" id="radio3" />
-        <input type="radio" name="radio-btn" id="radio4" />
-        <input type="radio" name="radio-btn" id="radio5" />
+            <div class="slide first">
+                <img src="../image/first-slide.jpeg" alt="" />
+            </div>
 
-        <div class="slide first">
-            <img src="../image/first-slide.jpeg" alt="" />
+            <div class="slide">
+                <img src="../image/Second-Silder.jpeg" alt="" />
+            </div>
+
+            <div class="slide">
+                <img src="../image/Third-Silder.png" alt="" />
+            </div>
+
+            <div class="slide">
+                <img src="../image/Fouth-Silder.jpeg" alt="" />
+            </div>
+
+            <div class="slide">
+                <img src="../image/Last-Silder.jpg" alt="" />
+            </div>
+
+            <div class="navigation-auto">
+                <div class="auto-btn1" />
+                <div class="auto-btn2" />
+                <div class="auto-btn3" />
+                <div class="auto-btn4" />
+                <div class="auto-btn5" />
+            </div>
         </div>
 
-        <div class="slide">
-            <img src="../image/Second-Silder.jpeg" alt="" />
+        <div class="navigation-manual">
+            <label for="radio1" class="manual-btn" />
+            <label for="radio2" class="manual-btn" />
+            <label for="radio3" class="manual-btn" />
+            <label for="radio4" class="manual-btn" />
+            <label for="radio5" class="manual-btn" />
         </div>
-
-        <div class="slide">
-            <img src="../image/Third-Silder.png" alt="" />
-        </div>
-
-        <div class="slide">
-            <img src="../image/Fouth-Silder.jpeg" alt="" />
-        </div>
-
-        <div class="slide">
-            <img src="../image/Last-Silder.jpg" alt="" />
-        </div>
-
-        <div class="navigation-auto">
-            <div class="auto-btn1" />
-            <div class="auto-btn2" />
-            <div class="auto-btn3" />
-            <div class="auto-btn4" />
-            <div class="auto-btn5" />
-        </div>
-
     </div>
-
-    <div class = "navigation-manual">
-        <label for = "radio1" class = "manual-btn"></label>
-        <label for = "radio2" class = "manual-btn"></label>
-        <label for = "radio3" class = "manual-btn"></label>
-        <label for = "radio4" class = "manual-btn"></label>
-        <label for = "radio5" class = "manual-btn"></label>
-    </div>
-
 </div>
