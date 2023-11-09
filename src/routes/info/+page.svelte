9fe1e89c54685ca734f6c2f5d640e4ef6c104f4c
SupaHotBall <lees27@student.op.ac.nz>
2023-11-09 17:44:30 +1300

    created the class called navigation-auto

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -6,25 +6,32 @@
         <input type="radio" name="radio-btn" id="radio4" />
         <input type="radio" name="radio-btn" id="radio4" />
 
-        <div class = "slide first">
-            <img src="../image/first-slide.jpeg" alt="">
+        <div class="slide first">
+            <img src="../image/first-slide.jpeg" alt="" />
         </div>
 
-        <div class = "slide">
-            <img src="../image/Second-Silder.jpeg" alt="">
+        <div class="slide">
+            <img src="../image/Second-Silder.jpeg" alt="" />
         </div>
 
-        <div class = "slide">
-            <img src="../image/Third-Silder.png" alt="">
+        <div class="slide">
+            <img src="../image/Third-Silder.png" alt="" />
         </div>
 
-        <div class = "slide">
-            <img src="../image/Fouth-Silder.jpeg" alt="">
+        <div class="slide">
+            <img src="../image/Fouth-Silder.jpeg" alt="" />
         </div>
 
-        <div class = "slide">
-            <img src="../image/Fifth-Silder.jpg" alt="">
+        <div class="slide">
+            <img src="../image/Fifth-Silder.jpg" alt="" />
         </div>
 
+        <div class="navigation-auto">
+            <div class="auto-btn1" />
+            <div class="auto-btn1" />
+            <div class="auto-btn1" />
+            <div class="auto-btn1" />
+            <div class="auto-btn1" />
+        </div>
     </div>
 </div>
