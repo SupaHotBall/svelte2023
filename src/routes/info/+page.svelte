df21d5ddca0659897b466654d40de9f70758b7dd
SupaHotBall <lees27@student.op.ac.nz>
2023-11-09 17:41:39 +1300

    put image path into the silde

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -4,5 +4,27 @@
         <input type="radio" name="radio-btn" id="radio2" />
         <input type="radio" name="radio-btn" id="radio3" />
         <input type="radio" name="radio-btn" id="radio4" />
+        <input type="radio" name="radio-btn" id="radio4" />
+
+        <div class = "slide first">
+            <img src="../image/first-slide.jpeg" alt="">
+        </div>
+
+        <div class = "slide">
+            <img src="../image/Second-Silder.jpeg" alt="">
+        </div>
+
+        <div class = "slide">
+            <img src="../image/Third-Silder.png" alt="">
+        </div>
+
+        <div class = "slide">
+            <img src="../image/first-slide.jpeg" alt="">
+        </div>
+
+        <div class = "slide">
+            <img src="../image/first-slide.jpeg" alt="">
+        </div>
+
     </div>
 </div>
