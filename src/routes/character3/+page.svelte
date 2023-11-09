41a03d02f92db478e792fc437092214b74c5ded1
SupaHotBall <lees27@student.op.ac.nz>
2023-11-09 13:53:53 +1300

    added the span name for each letter for the last character

--- src/routes/character3/+page.svelte
+++ src/routes/character3/+page.svelte
@@ -17,10 +17,10 @@
 
   function getVideoUrl(index) {
     const videoUrls = [
-      "https://www.youtube.com/embed/SQc2jhbGT2k?si=1DGQq0olblMVibZ3",
-      "https://www.youtube.com/embed/fueqERLu-Fk?si=Apns2jVo_RotE1GL&start=4",
-      "https://www.youtube.com/embed/ia6sRyT6ArE?si=fWr3a2GfZIYFC6nb&start=4",
-      "https://www.youtube.com/embed/pzIbP2AUXy8?si=SyqzzAsuVhhSISg2&start=4"
+      "https://www.youtube.com/embed/sy1Kdw1V0r0?si=r1rXZ0-II6G9EOvU",
+      "https://www.youtube.com/embed/UeOTYQwlUtA?si=ckft7GhTAi5IqG7v",
+      "https://www.youtube.com/embed/2V_O7lH3NRQ?si=8wTra3DCm97Il_YG",
+      "https://www.youtube.com/embed/SCAiB_AKu9U?si=e81cuSkyOAEdNOKB"
     ];
 
     if (index >= 0 && index < videoUrls.length) {
@@ -35,6 +35,17 @@
     <div class="Name">
     <h1>Kento</h1>
     <h1>Nanami</h1>
+    <span>K</span>
+    <span>E</span>
+    <span>N</span>
+    <span>T</span>
+    <span>O</span>
+    <span>N</span>
+    <span>A</span>
+    <span>N</span>
+    <span>A</span>
+    <span>M</span>
+    <span>I</span>
   </div>
 
   <div class="Video-container">
