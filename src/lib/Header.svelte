1b6b5d225c4367e1800f754726ee2dcc4ce13720
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-11-10 16:18:01 +1300

    added some comments

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -19,12 +19,19 @@
     
     <div class="header-right">
         
+        <!-- Character tab with a checkbox to toggle visibility -->
         <div class="characters-tab">
+            <!-- Checkbox input for toggling character menu visibility -->
             <input type="checkbox" id="active" {checked} on:change={e => { checked = !checked; }}>
+            <!-- When you click the "Character" label, it shows the character menu. -->
             <label for="active" on:click={hideMedia} class="glow-on-hover">Character</label>
+            <!-- when you click the "Character" label, it will close the menu -->
             <label for="active" on:click={displayMedia} class="close">X</label>
+
+            <!-- Character menu links with js clicking -->
             <div class="character-menu">
                 <div>
+                    <!-- Links for different characters, clicking on them toggles the checkbox state -->
                 <a href="/Gojo" on:click={e => { checked = !checked; }} class = "first-character">Gojo</a>
                 <a href="/Ryomen" on:click={e => { checked = !checked; }} class = "second-character">Ryomen</a>
                 <a href="/Kento" on:click={e => { checked = !checked; }} class = "third-character">Kento</a>
